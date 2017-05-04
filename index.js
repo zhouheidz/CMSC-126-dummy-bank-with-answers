17f3fd0721920d591176b1215e8c1a1b478a7a6f
Arnelle Balane <arnellebalane@gmail.com>
2017-05-05 02:31:26 +0800

    sign in with twitter using passportjs

--- index.js
+++ index.js
@@ -4,9 +4,35 @@ const cookieparser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('express-flash');
 const consolidate = require('consolidate');
+const passport = require('passport');
+const TwitterPassport = require('passport-twitter');
 const database = require('./database');
 const User = require('./models').User;
 const Account = require('./models').Account;
+
+// Configure PassportJS
+passport.use(new TwitterPassport({
+    consumerKey: '7mNd39P1eKcfpBF42skNxU6gV',
+    consumerSecret: 'ng5453RTqS1ltO7AWyowl53RYk6KMqqRK72gpOq5Plm7QRmME0',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback'
+}, function(token, secret, profile, cb) {
+    User.findOrCreate({
+        where: { email: profile.username },
+        defaults: { password: '' }
+    }).then(function(result) {
+        cb(null, result[0]);
+    });
+}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findOne({ where: { id: id } }).then(function(user) {
+        done(null, user);
+    });
+});
 
 const app = express();
 
@@ -17,6 +43,8 @@ app.use(bodyparser.urlencoded());
 app.use(cookieparser('secret-cookie'));
 app.use(session({ resave: false, saveUninitialized: false, secret: 'secret-cookie' }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/static', express.static('./static'));
 app.use(require('./auth-routes'));
@@ -60,6 +88,17 @@ app.post('/transfer', requireSignedIn, function(req, res) {
 		});
 	});
 });
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', {
+        failureRedirect: '/'
+    }),
+    function(req, res) {
+        req.session.currentUser = req.user.email;
+        res.redirect('/profile');
+    }
+);
 
 function requireSignedIn(req, res, next) {
     if (!req.session.currentUser) {
