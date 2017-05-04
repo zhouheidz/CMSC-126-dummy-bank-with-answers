6b7553dd4fa188f27855927c41540f522535f397
Arnelle Balane <arnellebalane@gmail.com>
2017-05-04 21:22:25 +0800

    define Account sequelize model

--- models.js
+++ models.js
@@ -21,4 +21,27 @@ const User = database.define('users', {
 	timestamps: true
 });
 
+const Account = database.define('accounts', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    balance: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0
+    },
+    user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    }
+}, {
+    timestamps: true
+});
+
 module.exports.User = User;
+module.exports.Account = Account;
