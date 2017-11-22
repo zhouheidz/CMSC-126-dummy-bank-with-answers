0fc9b9f7df621dd6177c12a932b98d9abfbdbfa5
zhouheidz <zhouheidz@gmail.com>
2017-11-23 01:37:32 +0800

    avoid sequelize error

--- database.js
+++ database.js
@@ -1,7 +1,19 @@
 const Sequelize = require('sequelize');
 
 const connectionUrl = 'postgres://bankuser:bankpassword@localhost:5432/bankdb';
-const database = new Sequelize(connectionUrl);
+var database = new Sequelize(connectionUrl);
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  database = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  var database = new Sequelize(connectionUrl);
+}
 
 database.sync();
 module.exports = database;
