2cab1199771ab000dbebdaa6d3fad6f14c24f1e9
zhouheidz <zhouheidz@gmail.com>
2018-04-10 16:40:31 +0800

    added more descriptive content for tests

--- tests/00-Setup.js
+++ tests/00-Setup.js
@@ -3,60 +3,60 @@ const assert = require('chai').assert;
 describe('Initial Setup must install the following dependencies:', function() {
     const dependencies = require('../package.json').dependencies;
 
-    it('"express"', function() {
+    it('"express", the web framework based on nodejs', function() {
         assert.property(dependencies, 'express');
         assert.doesNotThrow(() => require('express'));
     });
 
-    it('"body-parser"', function() {
+    it('"body-parser", for parsing http request bodies', function() {
         assert.property(dependencies, 'body-parser');
         assert.doesNotThrow(() => require('body-parser'));
     });
 
-    it('"nunjucks"', function() {
+    it('"nunjucks", for rendering templates', function() {
         assert.property(dependencies, 'nunjucks');
         assert.doesNotThrow(() => require('nunjucks'));
     });
 
-    it('"consolidate"', function() {
+    it('"consolidate", to make nunjucks conpatible with express', function() {
         assert.property(dependencies, 'consolidate');
         assert.doesNotThrow(() => require('consolidate'));
     });
-    it('"cookie-parser"', function() {
+    it('"cookie-parser", for parsing cookies in the header', function() {
         assert.property(dependencies, 'cookie-parser');
         assert.doesNotThrow(() => require('cookie-parser'));
     });  
-    it('"mocha"', function() {
+    it('"mocha", testing framework for Node.js', function() {
         assert.property(dependencies, 'mocha');
         assert.doesNotThrow(() => require('mocha'));
     }); 
-    it('"chai"', function() {
+    it('"chai", for asserting values against expected outputs', function() {
         assert.property(dependencies, 'chai');
         assert.doesNotThrow(() => require('chai'));
     });  
-    it('"axios"', function() {
+    it('"axios", to simplify HTTP requests', function() {
         assert.property(dependencies, 'axios');
         assert.doesNotThrow(() => require('axios'));
     });          
-    it('"sinon"', function() {
+    it('"sinon", for writing JavaScript unit tests', function() {
         assert.property(dependencies, 'sinon');
         assert.doesNotThrow(() => require('sinon'));
     });  
-    it('"stubs"', function() {
+    it('"stubs", to avoid writing the same code again and again', function() {
         assert.property(dependencies, 'stubs');
         assert.doesNotThrow(() => require('stubs'));
     });   
-    it('"passport"', function() {
+    it('"passport", for simplifying authentication requests', function() {
         assert.property(dependencies, 'passport');
         assert.doesNotThrow(() => require('passport'));
     });   
 
-    it('"pg"', function() {
+    it('"pg", provides access to PostgreSQL commands', function() {
         assert.property(dependencies, 'pg');
         assert.doesNotThrow(() => require('pg'));
     });   
 
-    it('"sequelize"', function() {
+    it('"sequelize", an ORM tool for Node.js', function() {
         assert.property(dependencies, 'sequelize');
         assert.doesNotThrow(() => require('sequelize'));
     });   
